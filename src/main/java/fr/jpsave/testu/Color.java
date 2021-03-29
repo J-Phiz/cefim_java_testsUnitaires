7cddb04c60af1fda8513be66f0ac698193a7e4ed
J-Phiz <jp.save@gmail.com>
2021-03-29 13:42:08 +0200

    Tests des constructeurs

--- src/main/java/fr/jpsave/testu/Color.java
+++ src/main/java/fr/jpsave/testu/Color.java
@@ -1,5 +1,7 @@
 package fr.jpsave.testu;
 
+import java.util.Locale;
+
 public class Color {
 
     private int red;
@@ -7,12 +9,39 @@ public class Color {
     private int blue;
     private String code;
 
-    public Color(int red, int green, int blue) {
+    private boolean checkColorNumber(int number) {
+        return number >= 0 && number <= 0xFF;
+    }
 
+    public Color(int red, int green, int blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+
+        if(!checkColorNumber(red) || !checkColorNumber(green) || !checkColorNumber(blue)) {
+            throw new IllegalArgumentException(
+                    "Le format d'un composant d'une couleur doit être un nombre entre 0 et 255"
+            );
+        }
+
+        this.code = String.format("#%06X", (red << 16) + (green << 8) + blue);
     }
 
     public Color(String rgbCode) {
+        if (rgbCode == null || rgbCode.isEmpty() || rgbCode.charAt(0) != '#' || rgbCode.length() != 7) {
+            throw new IllegalArgumentException(
+                    "Le format d'une couleur doit être #XXXXXX où X est un nombre compris entre 0 et F"
+            );
+        }
 
+        this.code = rgbCode.toUpperCase(Locale.ROOT);
+
+
+        int code = Integer.parseInt(rgbCode.substring(1), 16);
+
+        this.red = (code & 0x00FF0000) >> 16;
+        this.green = (code & 0x0000FF00) >> 8;
+        this.blue = code & 0x000000FF;
     }
 
     public int getRed() {
--- src/test/java/ColorTests.java
+++ src/test/java/ColorTests.java
@@ -14,29 +14,76 @@ public class ColorTests {
 
     @BeforeEach
     void setUpTests() {
-        System.out.println("Lancement d'un Test");
+        color = new Color(213, 141, 53);
     }
 
     @Test
-    void jUnitTestCase() {
-        System.out.println("Je suis dans le test");
-        assertNull(color);
+    void separateColorsConstructorOKTestCase() {
+        color = new Color(233, 11, 99);
+        assertEquals(233, color.getRed());
+        assertEquals(11, color.getGreen());
+        assertEquals(99, color.getBlue());
+        assertEquals("#E90B63", color.getRgbCode());
     }
 
     @Test
-    void jUnit2TestCase() {
-        assertNotNull(color);
-        System.out.println("Je suis dans le test2");
+    void hexaColorsConstructorOKTestCase() {
+        color = new Color("#D58d35");
+        assertEquals(213, color.getRed());
+        assertEquals(141, color.getGreen());
+        assertEquals(53, color.getBlue());
+        assertEquals("#D58D35", color.getRgbCode());
+    }
+
+    @Test
+    void separateColorsConstructorKOTestCase() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            color = new Color(257, 12, 43);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            color = new Color(253, 0x0FFE, 43);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            color = new Color(251, 12, 0x100);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            color = new Color(-3, 12, 43);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            color = new Color(253, -23, 43);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            color = new Color(25, 12, 0xFFFFFFFE);
+        });
+    }
+
+    @Test
+    void hexaColorsConstructorKOTestCase() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            color = new Color("RougeLeger;VertMoyen;BlueSombre");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            color = new Color("%d345F8");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            color = new Color("#d345f34");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            color = new Color("#d345f");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            color = new Color("#d3y5fG");
+        });
     }
 
     @AfterEach
     void tearDownTests() {
-        System.out.println("Fin du Test");
+        color = null;
     }
 
     @AfterAll
     static void afterClassTests() {
-        System.out.println("Dommage c'est déjà fini...");
+        System.out.println("Whouah c'est déjà fini...");
     }
 
 }
